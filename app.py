a3384cdc3fa5b9faa92f9c91c9951c4c255d5666
Gabriel <gabrieldev46@gmail.com>
2025-08-14 09:13:12 -0300

    Adicionado endpoint raiz para corrigir erro 404

--- app.py
+++ app.py
@@ -4,18 +4,21 @@ from routers.alunos import alunos_router
 from routers.cursos import cursos_router
 from routers.matriculas import matriculas_router
 
-
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
-    title="API de Gestão Escolar", 
+    title="API de Gestão Escolar",
     description="""
-        Esta API fornece endpoints para gerenciar alunos, cursos e turmas, em uma instituição de ensino.  
+        Esta API fornece endpoints para gerenciar alunos, cursos e turmas, em uma instituição de ensino.
         
         Permite realizar diferentes operações em cada uma dessas entidades.
-    """, 
+    """,
     version="1.0.0",
 )
+
+@app.get("/")
+async def root():
+    return {"message": "Bem-vindo à API de Gestão Escolar"}
 
 app.include_router(alunos_router, tags=["alunos"])
 app.include_router(cursos_router, tags=["cursos"])
